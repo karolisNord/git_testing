44b8efa05cc27051482a995a9a4da87df7a69724
karolisNord <karolis@nordclinic.com>
2016-02-11 13:33:14 +0200

    masterio

--- testing/ViewController.swift
+++ testing/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Is masterio bsk padirbejau")
     }
 
     override func didReceiveMemoryWarning() {
